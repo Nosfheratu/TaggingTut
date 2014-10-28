293f8586f913b487a074e89f1800b56c71cd3874
Fernando Serapio <fhernandosc@gmail.com>
2014-10-28 10:54:37 -0600

    implement controller's index and create methods, fix strong parameters

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,12 +1,25 @@
 class PostsController < ApplicationController
   def index
+    if params[:tag]
+      @posts = Post.tagged_with(params[:tag])
+    else
+      @posts = Post.all
+    end
   end
 
   def create
+    @post = Post.new(post_params)
+    respond_to do |format|
+      if @post.save
+        format.js
+      else
+        format.html { render root_path }
+      end
+    end
   end
 
   private
   def post_params
-    params.require(:event).permit(:author, :content, :all_tags)
+    params.require(:post).permit(:author, :content, :all_tags)
   end
 end
